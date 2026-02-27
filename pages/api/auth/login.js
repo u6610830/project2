39af8443f58dcbefe29357e012d0a6d1d932b6b9
Voramet Borvornsirisan <u6610830@au.edu>
2026-02-27 20:05:58 +0700

    Fix problem

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,5 +1,5 @@
 import connectDB from "../../../lib/mongodb";
-import { User } from "../../../models/User";
+import User  from "../../../models/User";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
